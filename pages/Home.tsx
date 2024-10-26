d470fe574913a1cc435478f4b3f79a82cfe54971
RyanGormican <ryan.gormican@gmail.com>
2024-10-26 05:25:52 -0600

    a
    a

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   const [isDraggingWidth, setIsDraggingWidth] = useState(false);
   const [isDraggingHeight, setIsDraggingHeight] = useState(false);
   const [currentMode, setCurrentMode] = useState('Container'); 
+  const [content, setContent] = useState('Resizable Container');
 
   const handleMouseDownWidth = () => {
     setIsDraggingWidth(true);
@@ -51,8 +52,14 @@ export default function Home() {
     };
   }, [isDraggingWidth, isDraggingHeight]);
 
-  const toggleMode = () => {
-    setCurrentMode((prevMode) => (prevMode === 'Container' ? 'Modules' : 'Container'));
+  // Function to set current mode
+  const toggleMode = (mode) => {
+    setCurrentMode(mode);
+  };
+
+  // Function to handle input change
+  const handleInputChange = (e) => {
+    setContent(e.target.value);
   };
 
   return (
@@ -74,11 +81,28 @@ export default function Home() {
         </div>
         <hr className="divider" />
         <div className="buttons flex">
-          <Icon icon="icon-park-outline:page" width="40" onClick={toggleMode} />
-          <Icon icon="fluent-mdl2:page-list" width="40" onClick={toggleMode} />
+          <Icon 
+            icon="icon-park-outline:page" 
+            width="40" 
+            onClick={() => toggleMode('Container')} 
+          />
+          {/*
+          <Icon 
+            icon="fluent-mdl2:page-list" 
+            width="40" 
+            onClick={() => toggleMode('Modules')} 
+          />
+          */}
+          <Icon 
+            icon="subway:write" 
+            width="40" 
+            onClick={() => toggleMode('Content')} 
+          />
         </div>
         <hr className="divider" />
-        {currentMode === 'Container' ? (
+        
+        {/* Container Mode */}
+        {currentMode === 'Container' && (
           <div style={{ display: 'flex', height: '87vh' }}>
             <div
               style={{
@@ -88,7 +112,7 @@ export default function Home() {
                 position: 'relative',
               }}
             >
-              Resizable Container
+              {content}
               <div
                 style={{
                   width: '2px', 
@@ -115,10 +139,26 @@ export default function Home() {
               />
             </div>
           </div>
-        ) : (
+        )}
+
+        {/* Modules Mode */}
+        {currentMode === 'Modules' && (
           <div style={{ height: '87vh', backgroundColor: '#e0e0e0', padding: '20px' }}>
-            <h2></h2>
+            <h2>Modules</h2>
       
+          </div>
+        )}
+
+        {/* Content Mode */}
+        {currentMode === 'Content' && (
+          <div style={{ height: '87vh', backgroundColor: '#e0e0e0', padding: '20px' }}>
+            <h2>Edit Container Text</h2>
+            <input 
+              type="text" 
+              value={content} 
+              onChange={handleInputChange} 
+              style={{ width: '100%', padding: '10px' }} 
+            />
           </div>
         )}
       </div>
