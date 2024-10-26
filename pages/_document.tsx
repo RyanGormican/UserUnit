424ab67810613e02bbeac5cdbf3d71aa2203da37
RyanGormican <ryan.gormican@gmail.com>
2024-10-26 05:47:28 -0600

    a
    a

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Doppio+One&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Jost:ital,wght@1,300&family=Lato&family=Oswald:wght@500&family=Sora:wght@100..800&family=Truculenta:opsz,wght@12..72,100..900&display=swap" rel="stylesheet" />
           <link rel="icon" href="/favicon.ico" />
           <meta name="google-site-verification" content="5XMOPXEjEeM6FSYUZd2l7jTdGAvbCWvFtlwUONQlpFU" />
+          <title> UserUnit</title> 
         </Head>
         <body>
           <Main />
